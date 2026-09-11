948f46b6f2a64e7429e1a77240fc2c2c91b96be3
Arpit-Rajawat <Arpitsingh62650@gmail.com>
2026-09-11 08:36:00 +0530

    update the main file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.config import Settings
+from app.api.routes_repo import router as repo_router
 
 
 
@@ -10,3 +11,7 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return {"status": "ok", "app": Settings.app_name}
+
+
+
+app.include_router(repo_router)
